ce878c826ef7bf16ce2ac05db791a47919804566
Harishkumarharry <54526806+Harishkumarharry@users.noreply.github.com>
2021-05-22 21:33:37 +0530

    2nd Final Commit
    Added 2 new C# scripts. Added OnCollisionEnter function. Fixed small bugs. And made some obstacles assest.

--- 2_Obstacle_Course_2021/Assets/Mover.cs
+++ 2_Obstacle_Course_2021/Assets/Mover.cs
@@ -9,16 +9,29 @@ public class Mover : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        PrintInstructions();
     }
 
     // Update is called once per frame
     void Update()
     {
+        MovePlayer();
+    }
+
+    void PrintInstructions()
+    {
+        Debug.Log("Welcome to the game.");
+        Debug.Log("Objective of the game is need to avoid obstacles and reach end point to finish/win the game.");
+        Debug.Log("Use 'WASD' or arrow keys to move Ninja.");
+    }
+
+    void MovePlayer()
+    {
         // Input.GetAxis is for getting input from any input device
         float xValue = Input.GetAxis("Horizontal") * Time.deltaTime * moveSpeed;
         float zValue = Input.GetAxis("Vertical") * Time.deltaTime * moveSpeed;
 
-        transform.Translate(xValue,0,zValue);
+        transform.Translate(xValue, 0, zValue);
     }
+
 }
